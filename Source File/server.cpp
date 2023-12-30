45e0bc202e1199d259d03ee9ef1ef4c41e47994c
harshuuanand <harshanand427@yahoo.com>
2023-12-31 05:10:18 +0530

    Update server.cpp

--- Source File/server.cpp
+++ Source File/server.cpp
@@ -1,10 +1,27 @@
-//
-//  server.cpp
-//  SocketChatRoom
-//
-//  Created by Harsh Anand on 28/12/2023.
-//
-//
+/* MIT License
+
+Copyright (c) 2023 Harsh Anand
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
+
 //=====================SERVER SIDE=========================//
 
 #include <iostream>
@@ -15,11 +32,9 @@
 #include <cstring>
 #include <thread>
 #include <chrono>
+#include <poll.h>
+#include <sys/select.h>
 
-void msgSend   (int*);
-void msgrecv   (int*);
-void savedmsg  (int*);
-int *acceptFxn (int *, struct sockaddr*, int *);
 //int ClientServerFileDiscription= accept(serverSocketFileDiscription,(struct sockaddr*)&address, &addressLen);\
 
 
@@ -37,162 +52,216 @@ struct Datails {
   char msgRecv[1024];
   char msgSend[1024];
   Datails(){
-     this->serverSocketFileDiscription=-1;
-     this->clientFileDiscription=-1;
-     this->NumberOfClient=0;
-     this->backlog=10;
-     this->port=6969;
-     this->addressFamily=AF_INET;
-     memset(this->name, '\0', 1024);
-     memset(this->msgRecv, '\0', 1024);
-     memset(this->msgSend, '\0', 1024);
+       this->serverSocketFileDiscription=-1;
+       this->clientFileDiscription=-1;
+       this->NumberOfClient=0;
+       this->backlog=10;
+       this->port=6969;
+       this->addressFamily=AF_INET;
+       memset(this->name, '\0', 1024);
+       memset(this->msgRecv, '\0', 1024);
+       memset(this->msgSend, '\0', 1024);
   }
 };
+
+
 struct ClientDetails{
   int ClientSocketFileDiscription;
+  int serverSocketFileDiscription;
+  int whichClient[20];
   int NumberOfClient;
   char msgSend[1024];
   char MsgResv[2048];
+  int tempfd;
   ClientDetails( int fd){
-     this->ClientSocketFileDiscription=fd;
-     this->NumberOfClient=0;
-     memset(this->MsgResv, '\0', 1023);
-     memset(this->msgSend, '\0', 1023);
+       this->ClientSocketFileDiscription=fd;
+       this->tempfd=-1;
+       this->NumberOfClient=0;
+       memset(this->whichClient, -1, 20);
+       memset(this->MsgResv, '\0', 1023);
+       memset(this->msgSend, '\0', 1023);
   }
 };
 
-
 typedef struct ClientDetails *CliDetails;
 
+
+void msgSend           (CliDetails);
+void msgrecv           (CliDetails);
+void savedmsg          (CliDetails);
+void messageBroadcast  (CliDetails);
+int *acceptFxn (int *, struct sockaddr*, int *);
+
+
 int main(void){
-   int serverSocketFileDiscription= socket(AF_INET, SOCK_STREAM, 0);
-   if (serverSocketFileDiscription<0){
-      perror("socket creation error\n");
-      exit(1);
-   }
 
-   struct  sockaddr_in address;
-   address.sin_family=AF_INET;
-   address.sin_port= htons(port);
-   inet_pton(AF_INET,"172.16.56.159",&address.sin_addr.s_addr);
-   int BindResult= bind(serverSocketFileDiscription, (struct sockaddr*) &address,sizeof address);
+     int serverSocketFileDiscription= socket(AF_INET, SOCK_STREAM, 0);
+     if (serverSocketFileDiscription<0){
+          perror("socket creation error\n");
+          exit(1);
+     }
 
-   //  BindResult?exit(2):printf("binding sucessful\n");
-   if (BindResult==0){
-      printf("binding sucessful\n");
-   }else{
-      perror("biding unsucessful\n");
-      exit(2);
-   }
+     struct  sockaddr_in address;
+     address.sin_family=AF_INET;
+     address.sin_port= htons(port);
+     inet_pton(AF_INET,"172.16.56.159",&address.sin_addr.s_addr);
+     int BindResult= bind(serverSocketFileDiscription, (struct sockaddr*) &address,sizeof address);
 
-   int listenRusult = listen(serverSocketFileDiscription, backlog);
+     //  BindResult?exit(2):printf("binding sucessful\n");
+     if (BindResult==0){
+          printf("binding sucessful\n");
+     }else{
+          perror("biding unsucessful\n");
+          exit(2);
+     }
 
-   if (listenRusult==0){
-      printf("listning sucessful\n");
-   }else{
-      perror("listning unsucessful\n");
-      exit(3);
-   }
+     int listenRusult = listen(serverSocketFileDiscription, backlog);
 
+     if (listenRusult==0){
+          printf("listning sucessful\n");
+     }else{
+          perror("listning unsucessful\n");
+          exit(3);
+     }
 
+     CliDetails clientDetails = new ClientDetails(0);
 
+     while (true) {
+          socklen_t addressLen= sizeof address;
+          clientDetails->ClientSocketFileDiscription= accept(serverSocketFileDiscription,(struct sockaddr*)&address, &addressLen);
+          clientDetails->whichClient[clientDetails->NumberOfClient++]=clientDetails->ClientSocketFileDiscription;
 
-//     char buffer[1024];
-//     memset(buffer, '\0', 1024);
-//     read(ClientServerFileDiscription,buffer, 1023); // -1 for null char
-   CliDetails clientDetails = new ClientDetails(0);
+          printf("New client joined the server\n");
 
-   int numberofclient=1;
-   while (true) {
-      socklen_t addressLen= sizeof address;
+          if (clientDetails->ClientSocketFileDiscription>0) {
+               std::thread  SendThread(msgSend , clientDetails);
+               std::thread  resvThread(msgrecv , clientDetails);
+               SendThread.detach();
+               resvThread.detach();
+          }else{
+               perror("error in acceptng incoming conection\n");
+               errno;
+               break;
+          }
+     }
 
-      clientDetails->ClientSocketFileDiscription= accept(serverSocketFileDiscription,(struct sockaddr*)&address, &addressLen);
+     /* clean up */
 
-      if (clientDetails->ClientSocketFileDiscription>0) {
-         numberofclient++;
-         std::thread  SendThread(msgSend , clientDetails);
-         std::thread  resvThread(msgrecv , clientDetails);
-         SendThread.detach();
-         resvThread.detach();
-      }else{
-         perror("error in acceptng incoming conection\n");
-         errno;
-         break;
-      }
-   }
-
-
-
-
-   close(serverSocketFileDiscription);
-   shutdown(serverSocketFileDiscription, SHUT_RDWR);
-   close(clientDetails->ClientSocketFileDiscription);
-   shutdown(clientDetails->ClientSocketFileDiscription, SHUT_RDWR);
-   return 0;
+     close(serverSocketFileDiscription);
+     shutdown(serverSocketFileDiscription, SHUT_RDWR);
+     close(clientDetails->ClientSocketFileDiscription);
+     shutdown(clientDetails->ClientSocketFileDiscription, SHUT_RDWR);
+     return 0;
 }
 
 void msgSend(CliDetails client){
-   char name[1024]="server ";
-   char msg[1024];
-   uint64_t namelen= strlen(name);
+     char name[1024]="";
+     char msg[1024];
+     uint64_t namelen= strlen(name);
 
-   while(true) {
-      fgets(msg, 1024, stdin);
-      printf("msg: %s\n", msg);
-      if (strcmp(msg, "exit")==0) {
-         break;
-      }
-      if (strlen(msg)>1) {
-         strcat(name, ": ");
-         strcat(name, msg );
-         strcpy(client->msgSend, name);
-         send(client->ClientSocketFileDiscription, name, strlen(name), 0);
-      }
-      for (int i=0;msg[i]; i++) {
-         name[i+namelen]='\0';
-      }
-   }
-   return;
+     while(true) {
+          fgets(msg, 1024, stdin);
+          int x=strcmp(msg, "exit\n");
+          if (x==0) {
+               printf("shutting down....\n");
+               close(client->serverSocketFileDiscription);
+               shutdown(client->serverSocketFileDiscription, SHUT_RDWR);
+               for (int i=0; i<client->NumberOfClient; ++i) {
+                    close(client->whichClient[i]);
+                    shutdown(client->whichClient[i], SHUT_RDWR);
+               }
+               exit(EXIT_SUCCESS);
+          }
+          if (strlen(msg)>1) {
+               strcat(name, " ");
+               strcat(name, msg );
+               strcpy(client->msgSend, name);
+               for (int i=0; i<client->NumberOfClient; ++i)
+                    send(client->whichClient[i], name, strlen(name), 0);
+          }
+          for (int i=0;msg[i]; i++) {
+               name[i+namelen]='\0';
+          }
+     }
+     return;
 }
+
 
 
 void msgrecv(CliDetails client){
-   int n=2;
-   char revMsgBuffer[1024];
-   memset(revMsgBuffer, '\0', 1024);
-   int64_t readbyte=0;
-   while (true) {
-      readbyte=read(client->ClientSocketFileDiscription, revMsgBuffer, 1023);
-      if (readbyte<=0) {
-         break;;
-      }else if (readbyte==1){
-         continue;
-      }else{
-         strcpy(client->MsgResv, revMsgBuffer);
-         printf("%s\n", revMsgBuffer);
-      }
-   }
-   return;
+     char revMsgBuffer[1024];
+     memset(revMsgBuffer, '\0', 1024);
+//     int64_t readbyte=0;
+     struct pollfd fds[20];
+     fds[0].fd= client->serverSocketFileDiscription;
+     fds[0].events=POLLIN;
+     while (true){
+          for (int i=0; i<client->NumberOfClient; ++i) {
+               if (client->whichClient[i]!=-1) {
+                    fds[i+1].fd=client->whichClient[i];
+                    fds[i+1].events=POLLIN;
+               }
+          }
+          int x=   poll(fds, client->NumberOfClient, -1);
+//          printf("<  %d > ", x);
+          for (int i=0; i<client->NumberOfClient; ++i) {
+               if (fds[i].revents&POLLIN) {
+                    char buffer[1024];
+                    int64_t byteread= recv(fds[i].fd, buffer, 1024, 0);
+                    if (byteread<0) {
+                         exit(5);
+                    }else if (byteread==1) continue;
+                    else{
+                         std::cout<<"msg: "<<buffer<<"\n";
+                         client->tempfd=fds[i].fd;
+                         strcpy(client->MsgResv, buffer);
+                         messageBroadcast(client);
+                         memset(buffer, '\0', 1024);
+                    }
+               }
+          }
+
+
+
+     }
+
+
+
+//     while (true) {
+//               readbyte=recv(client->whichClient[0], revMsgBuffer, 1023, 0);
+//          int pollResult= poll(client->whichClient, x, 500);
+////          printf("msg resv : client fd: %d\n", client->ClientSocketFileDiscription);
+//          if (readbyte<=0) {
+//               break;
+//          }else if (readbyte==1){
+//               continue;
+//          }else{
+//               strcpy(client->MsgResv, revMsgBuffer);
+//               messageBroadcast(client);
+//               printf("%s\n", revMsgBuffer);
+//          }
+//     }
+//     return;
 }
 
 void messageBroadcast(CliDetails client){
-   for (int i=1; i<=client->NumberOfClient; ++i) {
-      send(client->ClientSocketFileDiscription, client->MsgResv, strlen(client->MsgResv), 0);
-   }
-   memset(client->MsgResv, '\0', 1024);
+     for (int i=0; i<client->NumberOfClient; ++i) {
+          if (client->whichClient[i]!=client->tempfd)
+               send(client->whichClient[i], client->MsgResv, strlen(client->MsgResv), 0);
+     }
+     memset(client->MsgResv, '\0', 1024);
 
-   return;
+     return;
 }
 
 //int ClientServerFileDiscription= accept(serverSocketFileDiscription,(struct sockaddr*)&address, &addressLen);
 int * acceptFxn( int * serverFileDiscription , struct sockaddr_in address){
-//     static bool first= true;
-   int clientServerFileDiscription;
-   socklen_t addressLen= sizeof(address);
-   while (true) {
-      clientServerFileDiscription=accept(*serverFileDiscription, (struct sockaddr *)&address, &addressLen);
+     int clientServerFileDiscription;
+     socklen_t addressLen= sizeof(address);
+     while (true) {
+          clientServerFileDiscription=accept(*serverFileDiscription, (struct sockaddr *)&address, &addressLen);
 
-      std::this_thread::sleep_for(std::chrono::milliseconds(500));
-   }
+          std::this_thread::sleep_for(std::chrono::milliseconds(500));
+     }
 
 }
